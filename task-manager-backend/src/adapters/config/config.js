025002e841cd5849252820f24e6a4195f4473e55
Victor Hugo (Torugo) Magalhães <hello@victordias.dev>
2022-11-09 09:31:07 -0300

    chore: fixing port mismatch with frontend application

--- task-manager-backend/src/adapters/config/config.js
+++ task-manager-backend/src/adapters/config/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const config = {
-  port: process.env.PORT || '5000',
+  port: process.env.PORT || '5001',
   tokenSecret: process.env.TOKEN_SECRET || 'secret',
   expirationTime: process.env.EXPIRATION_TIME || '1h'
 }
